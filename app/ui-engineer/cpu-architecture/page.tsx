6fd4daee7233897ce9f3f6a0d3bc22835862f3ad
NV-Phong <0h9ng03@gmail.com>
2025-05-30 01:17:41 +0700

    FEAT - update cpu architecture effect

--- app/ui-engineer/cpu-architecture/page.tsx
+++ app/ui-engineer/cpu-architecture/page.tsx
@@ -1,8 +1,10 @@
-import { CpuArchitecture } from '@/components/ui/cpu-architecture'
-import React from 'react'
+import { CpuArchitecture } from "@/components/ui/cpu-architecture";
+import React from "react";
 
 export default function CPUArchitecture() {
-  return (
-    <div><CpuArchitecture/></div>
-  )
+   return (
+      <div className="h-screen flex justify-center">
+         <CpuArchitecture className="w-70 "/>
+      </div>
+   );
 }
