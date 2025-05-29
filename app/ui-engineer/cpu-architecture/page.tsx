931b4725010e9bf8330dc43f4973dccb630f8904
NV-Phong <0h9ng03@gmail.com>
2025-05-30 00:44:33 +0700

    FEAT - add cpu architecture

---
+++ app/ui-engineer/cpu-architecture/page.tsx
@@ -0,0 +1,8 @@
+import { CpuArchitecture } from '@/components/ui/cpu-architecture'
+import React from 'react'
+
+export default function CPUArchitecture() {
+  return (
+    <div><CpuArchitecture/></div>
+  )
+}
