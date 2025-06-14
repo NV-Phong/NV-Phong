3e58cc81ae4eb97c7d52d801752c3111a53d8d3f
NV-Phong <0h9ng03@gmail.com>
2025-06-14 15:41:27 +0700

    FEAT - splash cursor

---
+++ app/ui-engineer/splash-cursor/page.tsx
@@ -0,0 +1,10 @@
+import { SplashCursor } from "@/components/ui/splash-cursor";
+import React from "react";
+
+export default function SplashCursorDemo() {
+   return (
+      <div>
+         <SplashCursor />
+      </div>
+   );
+}
