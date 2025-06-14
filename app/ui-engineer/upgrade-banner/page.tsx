34de0f585aac3e15c3ffbf482cff99c00b9f877f
NV-Phong <0h9ng03@gmail.com>
2025-06-14 15:35:24 +0700

    FEAT - upgrade banner

---
+++ app/ui-engineer/upgrade-banner/page.tsx
@@ -0,0 +1,12 @@
+import { UpgradeBanner } from "@/components/ui/upgrade-banner";
+import React from "react";
+
+export default function page() {
+   return (
+      <div>
+         <div className="flex justify-center items-center h-screen">
+            <UpgradeBanner />
+         </div>
+      </div>
+   );
+}
