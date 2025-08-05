aa8def169079df1e9cf7f07d397d0e6bde9bf0ef
NV-Phong <0h9ng03@gmail.com>
2025-08-05 13:52:31 +0700

    FEAT - preview resume mode

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,11 +1,22 @@
 "use client";
 import React, { useState } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Card } from "@/components/ui/card";
-import Resume from "@/components/ui-engineer/resume";
+import {
+   Card,
+   CardContent,
+   CardDescription,
+   CardFooter,
+   CardHeader,
+   CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { CommitChanges } from "@/components/ui-engineer/commit-changes";
+import CommitsPerDay from "@/components/ui-engineer/commits-per-day";
+import { Label } from "@/components/ui/label";
+import { DatePicker } from "@/components/ui-engineer/range-date-picker";
 
 export default function IntroCard() {
-   const [currentTab, setCurrentTab] = useState("light");
+   const [currentTab, setCurrentTab] = useState("preview");
    return (
       <div className="relative grid min-h-screen grid-cols-[1fr_2.5rem_auto_2.5rem_1fr] grid-rows-[1fr_1px_auto_1px_1fr] [--pattern-fg:var(--color-gray-950)]/5 dark:[--pattern-fg:var(--color-white)]/10">
          <Tabs
@@ -14,24 +25,61 @@ export default function IntroCard() {
             className="col-start-3 row-start-3 flex flex-col items-center z-1"
          >
             <TabsList className="bg-primary/15 border border-primary/20">
-               <TabsTrigger value="light" className="text-primary">
-                  Light
+               <TabsTrigger value="preview" className="text-primary">
+                  Preview
                </TabsTrigger>
-               <TabsTrigger value="dark" className="text-primary">
-                  Dark
+               <TabsTrigger value="download" className="text-primary">
+                  Download
                </TabsTrigger>
             </TabsList>
 
-            <div className="flex max-w-lg flex-col bg-primary/20 p-2 dark:bg-white/10">
-               <TabsContent value="light" className="flex justify-center">
-                  <Card className="rounded-xl bg-card p-10 text-sm/7 text-foreground shadow-none border-none">
-                     <Resume/>
+            <div className="flex flex-col bg-primary/20 p-2 dark:bg-white/10">
+               <TabsContent
+                  forceMount={true}
+                  value="preview"
+                  className="flex justify-center data-[state=inactive]:hidden"
+               >
+                  <Card className="rounded-xl bg-card p-0 text-sm/7 text-foreground shadow-none border-none">
+                     <iframe
+                        className="rounded-xl border border-primary/30"
+                        width="1000"
+                        height="562"
+                        src="https://embed.figma.com/design/rkiZnUdOKwzcGShxZHzAWs/CV-%E2%80%A2-Resume?node-id=0-1&embed-host=share"
+                        allowFullScreen
+                     ></iframe>
                   </Card>
                </TabsContent>
 
-               <TabsContent value="dark" className="flex justify-center">
-                  <Card className="rounded-xl bg-card p-10 text-sm/7 text-foreground shadow-none border-none">
-                     dark resume in here
+               <TabsContent value="download" className="flex justify-center">
+                  <Card className="w-xl rounded-xl bg-card border-none shadow-none">
+                     <CardHeader>
+                        <CardTitle className="text-xl">DOWNLOAD</CardTitle>
+                        <CardDescription>
+                           Download my resume in PDF format
+                        </CardDescription>
+                     </CardHeader>
+                     <CardContent className="space-y-4">
+                        <div className="space-y-2 flex justify-between">
+                           <div className="space-y-2">
+                              <Label>Light Resume</Label>
+                              <DatePicker />
+                           </div>
+                           <div className="space-y-2">
+                              <Label>Dark Resume</Label>
+                              <CommitsPerDay />
+                           </div>
+                        </div>
+                     </CardContent>
+                     <CardFooter className="align justify-between">
+                        <Button
+                           variant="outline"
+                           className="w-2/5 bg-transparent"
+                           type="button"
+                        >
+                           Clear
+                        </Button>
+                        <CommitChanges />
+                     </CardFooter>
                   </Card>
                </TabsContent>
             </div>
