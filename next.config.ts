e8f06e89f35a63b3538468b2da01a21b2a285be6
NV-Phong <0h9ng03@gmail.com>
2025-08-30 00:27:37 +0700

    FEAT - rewrites api routes 🧩

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   /* config options here */
+   async rewrites() {
+      return [
+         {
+            source: "/api/:path*",
+            destination: "/api/supabase/:path*",
+         },
+      ];
+   },
 };
 
 export default nextConfig;
