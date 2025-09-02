de1a19fb701d84ee4329b4d11ca536d72f5b5069
NV-Phong <0h9ng03@gmail.com>
2025-09-02 07:13:49 +0700

    DOCS - minimallist doc

--- app/api/mongodb/users/[id]/route.ts
+++ app/api/mongodb/users/[id]/route.ts
@@ -1,5 +1,5 @@
 import { connectDB } from "@/lib/mongoose";
-import { User } from "@/schema/user";
+import { User } from "@/schemas/user";
 import { NextResponse } from "next/server";
 
 type RouteContext = {
