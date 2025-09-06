e74bd808b06a0be9f4973cc7e4f7ea6b015682a2
NV-Phong <0h9ng03@gmail.com>
2025-09-06 13:20:40 +0700

    FEAT - google sign in by supabase

--- app/api/supabase/auth/sign-in/route.ts
+++ app/api/supabase/auth/sign-in/route.ts
@@ -3,12 +3,29 @@ import { createClient } from "@/utils/supabase/server";
 
 export async function POST(req: Request) {
    const supabase = await createClient();
-   const { email, password } = await req.json();
+   const { email, password, provider } = await req.json();
 
+   //-------------------------------------------------- EMAIL/PASSWORD --------------------------------------------------//
    const { data, error } = await supabase.auth.signInWithPassword({
       email,
       password,
    });
+
+   //-------------------------------------------------- GOOGLE --------------------------------------------------//
+   if (provider === "google") {
+      const { data, error } = await supabase.auth.signInWithOAuth({
+         provider: "google",
+         options: {
+            redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth`,
+         },
+      });
+
+      if (error) {
+         return NextResponse.json({ error: error.message }, { status: 400 });
+      }
+
+      return NextResponse.json({ data });
+   }
 
    if (error) {
       return NextResponse.json({ error: error.message }, { status: 400 });
--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -81,6 +81,26 @@ export default function Auth() {
       }
    }
 
+   async function handleGoogleSignIn() {
+      try {
+         setLoading(true);
+         const res = await fetch("/api/supabase/auth/sign-in", {
+            method: "POST",
+            body: JSON.stringify({ provider: "google" }),
+         });
+         const json = await res.json();
+         if (res.ok && json.data.url) {
+            window.location.href = json.data.url;
+         } else {
+            toast(json.error || "Google Sign-In failed", {
+               description: "Please try again !",
+            });
+         }
+      } finally {
+         setLoading(false);
+      }
+   }
+
    return (
       <div className="relative grid min-h-screen grid-cols-[1fr_2.5rem_auto_2.5rem_1fr] grid-rows-[1fr_1px_auto_1px_1fr] [--pattern-fg:var(--color-gray-950)]/5 dark:[--pattern-fg:var(--color-white)]/10">
          {showParticles && (
@@ -137,6 +157,7 @@ export default function Auth() {
                            <Button
                               variant={"ghost"}
                               className="border shadow-sm"
+                              onClick={handleGoogleSignIn}
                            >
                               <Icon styles="solid" name="google-logo-bold" />
                               Google
