217aaf5ba7421b3dcfe5ed80a493f8f1f00a09d2
NV-Phong <0h9ng03@gmail.com>
2025-09-15 15:43:07 +0700

    FEAT - add signature

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,42 @@ const nextConfig: NextConfig = {
    async rewrites() {
       return [
          {
+            source: "/api/v1/:path*",
+            destination: "/api/next/:path*",
+         },
+         {
             source: "/api/:path*",
             destination: "/api/supabase/:path*",
+         },
+         {
+            source: "/signature",
+            destination:
+               "https://qq90n93onn5q8tmx.public.blob.vercel-storage.com/Signature/Signature%20-%20Pink.png",
+         },
+         {
+            source: "/signature/green",
+            destination:
+               "https://qq90n93onn5q8tmx.public.blob.vercel-storage.com/Signature/Signature%20-%20Green.png",
+         },
+         {
+            source: "/signature/pink",
+            destination:
+               "https://qq90n93onn5q8tmx.public.blob.vercel-storage.com/Signature/Signature%20-%20Pink.png",
+         },
+         {
+            source: "/signature/blue",
+            destination:
+               "https://qq90n93onn5q8tmx.public.blob.vercel-storage.com/Signature/Signature%20-%20Blue.png",
+         },
+         {
+            source: "/signature/violet",
+            destination:
+               "https://qq90n93onn5q8tmx.public.blob.vercel-storage.com/Signature/Signature%20-%20Violet.png",
+         },
+         {
+            source: "/signature/orange",
+            destination:
+               "https://qq90n93onn5q8tmx.public.blob.vercel-storage.com/Signature/Signature%20-%20Orange.png",
          },
       ];
    },
